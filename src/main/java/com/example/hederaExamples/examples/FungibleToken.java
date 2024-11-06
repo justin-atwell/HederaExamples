5d15737647246bbd40a4811208bcfe20be43e1f2
unknown <jtatwell1@gmail.com>
2024-11-06 11:17:02 -0600

    main - showing hashgraph gradle and accountId usage for dotEnv

--- src/main/java/com/example/hederaExamples/examples/FungibleToken.java
+++ src/main/java/com/example/hederaExamples/examples/FungibleToken.java
@@ -1,15 +1,21 @@
 package com.example.hederaExamples.examples;
 
+import com.hedera.hashgraph.sdk.AccountId;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 import io.github.cdimascio.dotenv.Dotenv;
+
+import java.util.Objects;
 
 @Component
 public class FungibleToken {
 
     @EventListener(ApplicationReadyEvent.class)
     public void doItBoy(){
-        System.out.println("show me");
+
+        AccountId OPERATOR_ID = AccountId.fromString(Objects.requireNonNull(Dotenv.load().get("OPERATOR_ID")));
+
+        System.out.println(OPERATOR_ID);
     }
 }
