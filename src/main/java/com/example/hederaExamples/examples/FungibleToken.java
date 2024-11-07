6ac798e200a71a1489d8fe7770367a8671bfdc3b
unknown <jtatwell1@gmail.com>
2024-11-06 18:48:35 -0600

    accounts - naming

--- src/main/java/com/example/hederaExamples/examples/FungibleToken.java
+++ src/main/java/com/example/hederaExamples/examples/FungibleToken.java
@@ -12,7 +12,7 @@ import java.util.Objects;
 public class FungibleToken {
 
     @EventListener(ApplicationReadyEvent.class)
-    public void doItBoy(){
+    public void retrieveOperatorIdWithDotEnv(){
 
         AccountId OPERATOR_ID = AccountId.fromString(Objects.requireNonNull(Dotenv.load().get("OPERATOR_ID")));
 
